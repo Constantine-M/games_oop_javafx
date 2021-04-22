51e726d29ce39539814448d318046e54c03f4081
Constantine-M <mez365@mail.ru>
2021-04-22 15:55:04 +0300

    2. Каркас шахматной доски [#347632] - last correction

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -13,17 +13,6 @@ import static org.hamcrest.Matchers.is;
 
 public class LogicTest {
 
-    @Ignore
-    @Test
-    public void whenFindIndex()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
-        Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C1, Cell.H6);
-        Figure destPlace = new BishopBlack(Cell.H6);
-        //assertThat(logic.move(Cell.C1, Cell.H6), is(destPlace));
-    }
-
     @Test(expected = FigureNotFoundException.class)
     public void whenFigureNotFound()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
